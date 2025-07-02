08844626e139aae1a2421a132e120adc773f704c
Micah Resnick <resnickmicah@gmail.com>
2025-07-02 13:07:16 -0400

    Add internet input number spec

---
+++ tests/internet-input-number.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Input numerical values directly', () => {
+  [
+    { label: 'Life, the universe, and everything', val: 42 },
+    { label: 'Number of US states', val: 50 },
+    { label: 'The square of your imagination', val: -1}
+  ].forEach(({ label, val }) => {
+    test(label, async ({ page }) => {
+      await page.goto('http://the-internet.herokuapp.com/inputs');
+      const numInput = await page.getByRole('spinbutton');
+      await numInput.click();
+      await numInput.fill(val.toString());
+      await numInput.press('Enter');
+      await expect(numInput).toHaveValue(val.toString());
+    });
+  });
+});
+
+test('Increment numerical value with keyboard', async ({ page }) => {
+  await page.goto('http://the-internet.herokuapp.com/inputs');
+  const numInput = await page.getByRole('spinbutton');
+  await expect(numInput).toBeEmpty();
+  await numInput.click();
+  await numInput.press('ArrowUp');
+  await numInput.press('ArrowUp');
+  await numInput.press('ArrowUp');
+  await numInput.press('ArrowUp');
+  await expect(numInput).toHaveValue('4');
+});
