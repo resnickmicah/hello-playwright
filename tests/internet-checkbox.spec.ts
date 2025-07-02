f4b8a1097e8c574a1cc41db045793288c08c4bd4
Micah Resnick <resnickmicah@gmail.com>
2025-07-02 12:39:27 -0400

    Add internet checkbox spec

---
+++ tests/internet-checkbox.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('Toggle unchecked checkbox', async ({ page }) => {
+  await page.goto('http://the-internet.herokuapp.com/checkboxes');
+  await expect(page.getByRole('checkbox').first()).not.toBeChecked();
+  await page.getByRole('checkbox').first().check();
+  await expect(page.getByRole('checkbox').first()).toBeChecked();
+});
+
+test('Toggle checked checkbox', async ({ page }) => {
+  await page.goto('http://the-internet.herokuapp.com/checkboxes');
+  await expect(page.getByRole('checkbox').nth(1)).toBeChecked();
+  await page.getByRole('checkbox').nth(1).uncheck();
+  await expect(page.getByRole('checkbox').nth(1)).not.toBeChecked();
+});
